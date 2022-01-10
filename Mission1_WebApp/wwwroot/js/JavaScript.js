374132edb97acd69a09fc49d9b3e457e56a0dc86
markdvogt <90359765+markdvogt@users.noreply.github.com>
2022-01-10 10:33:44 -0700

    Add project files.

---
+++ Mission1_WebApp/wwwroot/js/JavaScript.js
@@ -0,0 +1,55 @@
+﻿var fAssignments = parseFloat(document.getElementById("assignments").value)
+var fGroupProjects = parseFloat(document.getElementById("group_projects").value)
+var fQuizzes = parseFloat(document.getElementById("quizzes").value)
+var fExams = parseFloat(document.getElementById("exams").value)
+var fIntex = parseFloat(document.getElementById("intex").value)
+
+var fFinalGrade = (fAssignments * 0.55) + (fGroupProjects * 0.05) + (fQuizzes * 0.1) + (fExams * 0.2) + (fIntex * 0.1)
+
+var sLetterGrade;
+
+if (fFinalGrade >= 94) {
+	sLetterGrade = "A"
+}
+else if (fFinalGrade >= 90) {
+	sLetterGrade = "A-"
+}
+else if (fFinalGrade >= 87) {
+	sLetterGrade = "B+"
+}
+else if (fFinalGrade >= 84) {
+	sLetterGrade = "B"
+}
+else if (fFinalGrade >= 80) {
+	sLetterGrade = "B-"
+}
+else if (fFinalGrade >= 77) {
+	sLetterGrade = "C+"
+}
+else if (fFinalGrade >= 74) {
+	sLetterGrade = "C"
+}
+else if (fFinalGrade >= 70) {
+	sLetterGrade = "C-"
+}
+else if (fFinalGrade >= 67) {
+	sLetterGrade = "D+"
+}
+else if (fFinalGrade >= 64) {
+	sLetterGrade = "D"
+}
+else if (fFinalGrade >= 60) {
+	sLetterGrade = "D-"
+}
+else {
+	sLetterGrade = "E"
+} 
+
+
+document.getElementById("calculate").addEventListener("click", function () {
+	alert("Your final grade is " + fFinalGrade + "% which is a(n) " + sLetterGrade + ".");
+}) 
+
+/* document.getElementById("calculate").addEventListener("click", function () {
+	alert("It Works!");
+}) */
---
+++ Mission1_WebApp/wwwroot/js/myWebsite.js
@@ -0,0 +1,56 @@
+﻿$("#calculate").click(function () {
+
+	var fAssignments = $("#assignments").val();
+	var fGroupProjects = $("#group_projects").val();
+	var fQuizzes = $("#quizzes").val();
+	var fExams = $("#exams").val();
+	var fIntex = $("#intex").val();
+
+	var fFinalGrade = (fAssignments * 0.55) +
+					  (fGroupProjects * 0.05) +
+					  (fQuizzes * 0.1) +
+					  (fExams * 0.2) +
+					  (fIntex * 0.1);
+
+	var sLetterGrade;
+
+	if (fFinalGrade >= 94) {
+		sLetterGrade = "A";
+	}
+	else if (fFinalGrade >= 90) {
+		sLetterGrade = "A-";
+	}
+	else if (fFinalGrade >= 87) {
+		sLetterGrade = "B+";
+	}
+	else if (fFinalGrade >= 84) {
+		sLetterGrade = "B";
+	}
+	else if (fFinalGrade >= 80) {
+		sLetterGrade = "B-";
+	}
+	else if (fFinalGrade >= 77) {
+		sLetterGrade = "C+";
+	}
+	else if (fFinalGrade >= 74) {
+		sLetterGrade = "C";
+	}
+	else if (fFinalGrade >= 70) {
+		sLetterGrade = "C-";
+	}
+	else if (fFinalGrade >= 67) {
+		sLetterGrade = "D+";
+	}
+	else if (fFinalGrade >= 64) {
+		sLetterGrade = "D";
+	}
+	else if (fFinalGrade >= 60) {
+		sLetterGrade = "D-";
+	}
+	else {
+		sLetterGrade = "E";
+	}
+
+	alert(fAssignments);
+	//alert("Your final grade is " + fFinalGrade + "% which is a(n) " + sLetterGrade + ".");
+})
